83c361c10dc2e84f95e3508fbadabf6a159b6e61
John Li <44646119+GuojunLee001@users.noreply.github.com>
2019-01-13 16:56:02 +0800

    习题 3: 数字和数学计算

---
+++ ex3.py
@@ -0,0 +1,62 @@
+﻿# _*_ coding : utf-8 _*_
+
+# 输出内容为文本
+print "I will now count my chickens:"
+
+# 输出内容为文本加数字，数字不用加""
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+# 输出内容为文本,
+print "Now I will count the eggs:"
+
+print 3+ 2 + 1 -5 + 4 % 2 - 1 / 4 + 6
+
+# 上面也可以写成，差别是输出的是不是在一行内
+print "Now I will count the eggs:", 3 + 2 + 1 -5 + 4 % 2 - 1 / 4 + 6
+
+# 判断对错
+print "Is it true that 3 + 2 < 5 - 7?"
+print 3 +2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2 
+print "What is 5 -7?", 5 - 7
+
+print "Oh, that's why it's Falss."
+
+# 不等号的练习
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <=-2
+
+# 加分题，浮点数代表数学中的小数
+"I will now count my chickens:"
+print "Hens", float(25 + 30 / 6)
+print "Roosters", 100 - float(25 * 3 % 4)
+
+print "Now I will count the eggs:"
+
+print float(3+ 2 + 1 -5 + 4 % 2 - 1 / 4 + 6)
+
+
+print "Is it true that 3 + 2 < 5 - 7?"
+print 3 +2 < 5 - 7
+
+print "What is 3 + 2?", float(3 + 2) 
+print "What is 5 -7?", float(5 - 7)
+
+print "Oh, that's why it's Falss."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+
+# 直接输入小数时能够计算
+print 5.2222 - 2.33
+print float(56) - float(2.33)
+
+
+
+print "Is it less or equal?", 5 <=-2
