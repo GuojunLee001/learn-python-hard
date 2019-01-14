616fdbaafbf60e9a5d6b4d1e329a9c8a33b8e29a
John Li <44646119+GuojunLee001@users.noreply.github.com>
2019-01-14 12:39:48 +0800

    习题 7: 更多打印

---
+++ ex7.py
@@ -0,0 +1,29 @@
+# _*_ coding: utf-8 _*_
+
+# 简答的输出
+print "Mary had little lamb."
+# 用字符串定义后输出,文本型字符串必须加''
+print "Its fleece was white as %s." % 'snow'
+# 简单的输出
+print "And everywhere that Mary went."
+# 输出10个.
+print "." * 10 # what'd that do?
+
+# 用变量定义每个字母
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# 输出单词，w + e 形式的输出可以把单词也可以把句子拼接在一起，如果是数字，则代表相加
+# whtch what camma at the end. try removing it to see what happens
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
