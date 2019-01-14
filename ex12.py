a9f15a82e1f0cf00e9d8313937aaebe5a1f24960
John Li <44646119+GuojunLee001@users.noreply.github.com>
2019-01-14 16:19:43 +0800

    习题 12: 提示别人

---
+++ ex12.py
@@ -0,0 +1,12 @@
+# _*_ coding: utf-8 _*_
+
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do you weigh? ")
+
+print "So, you're %r old, %r tall and %r heavy." % (
+  age, height, weight)
+  
+# 加分题
+# 运行pydoc 显示invalid syntax
+# pydoc 用法浏览了不是很清楚
