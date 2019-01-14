fdba9b20b769b63dd1befa240d7545e428bf2f9f
John Li <44646119+GuojunLee001@users.noreply.github.com>
2019-01-14 12:45:09 +0800

    习题 9: 打印，打印，打印

---
+++ ex9.py
@@ -0,0 +1,19 @@
+# _*_ coding: utf-8 _*_
+
+# Here's some new strange stuff, remember type it exactly.
+
+# 简单的输出
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# 加入\n 表示转义字符
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+print """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
+
