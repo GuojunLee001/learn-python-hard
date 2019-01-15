7865369ab8888868c5544c51be427b1061d34063
John Li <44646119+GuojunLee001@users.noreply.github.com>
2019-01-15 14:59:27 +0800

    习题 17: 更多文件操作

---
+++ ex17.py
@@ -0,0 +1,49 @@
+# coding utf-8
+
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv
+
+print "Copying from %s to %s" % (from_file, to_file)
+
+# We could do these two on one line too, how?
+in_file = open(from_file)
+indata = in_file.read()
+
+print "The input file is %d bytes long" % len(indata)
+
+print "Does the output file exist? %r" % exists(to_file)
+print "Ready, hit RETURN to continue, CTRL-C to abort."
+raw_input
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+print "Alright, all done."
+
+out_file.close()
+in_file.close()
+
+# 加分题
+from sys import argv
+from os.path import exists
+script, from_file, to_file = argv
+
+# We could do these two on one line too, how?
+in_file = open(from_file)
+indata = in_file.read()
+
+print "The input file is %d bytes long" % len(indata)
+
+print "Ready, hit RETURN to continue, CTRL-C to abort."
+raw_input
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+print "Alright, all done."
+
+# 关闭和保存写入的文档
+out_file.close()
+in_file.close()
