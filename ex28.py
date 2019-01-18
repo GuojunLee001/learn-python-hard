a697e8f04ffa9064ccb82c05551d71f06dda27f0
John Li <44646119+GuojunLee001@users.noreply.github.com>
2019-01-18 10:28:38 +0800

    习题 28: 布尔表达式练习

---
+++ ex28.py
@@ -0,0 +1,27 @@
+# coding:utf-8
+
+# Ture = 0, False = 1
+True and True # 0
+False and True # 1
+1 == 1 and 2 == 1 # 1
+"test" = "test" # 0
+1 == 1 or 2 != 1 # 0
+True and 1 ==1 # 0
+False and 0!= 0 # 1
+True or 1 == 1 # 0
+"test" = "testing" # 1
+1 != 0 and 2 == 1 # 1
+"test" != "testing" # 0
+"test" == 1 # 1
+not (True and False) # 0
+not (1 == 1 and 0!= 1) # 1
+not (10 == 1 or 1000 == 1000) # 1
+not (1 != 10 or 3 == 4) # 1
+not ("testing" == "testing" and "Zed" == "Cool Guy") # 0
+1 == 1 and (not ("testing" == 1 or 1 == 0)) # 0
+# "chunky" == "bacon" and (not(3 == 4 or 3 == 3)) # 1
+3 == 3 and (not ("testing" == "testing" or "Python" == "Fun")) # 1
+
+# 附加题
+# python 运算符总结：http://shouce.jb51.net/python-way/learn-python-the-hard-way-exercise28.html 具体复习见ex27.md
+
