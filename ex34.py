5a3b0f53bb0ee27877674bfb7db7238652aff4a0
John Li <44646119+GuojunLee001@users.noreply.github.com>
2019-01-23 10:38:01 +0800

    习题 34: 访问列表的元素

---
+++ ex34.py
@@ -0,0 +1,20 @@
+#coding:utf-8
+
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print "The 1st animal is at 0 and is a %r." % animals[0]
+print "The 4th animal is at 3 and is a %r." % animals[3]
+print "The 5th animal is at 4 and is a %r." % animals[4]
+print "The 6th animal is at 5 and is a %r." % animals[5]
+print "The 3rd animal is at 2 and is a %r." % animals[2]
+print "The 2nd animal is at 1 and is a %r." % animals[1]
+
+# 附加题
+numbers = [1, 2, 3, 4, 5, 6, 7]
+print "The 1rt numbers is at 0 is %r." % numbers[0]
+print "The 6th numbers is at 5 is %r." % numbers[5]
+print "The 7th numbers is at 6 is %r." % numbers[6]
+print "The 4th numbers is at 3 is %r." % numbers[3]
+print "The 3rd numbers is at 2 is %r." % numbers[2]
+print "The 2nd numbers is at 1 is %r." % numbers[1]
+print "The 5th numbers is at 4 is %r." % numbers[4]
