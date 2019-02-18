1d46f09be6b32f9227d1ad5469907e2ce2ead70c
John Li <44646119+GuojunLee001@users.noreply.github.com>
2019-02-18 10:59:24 +0800

    补充练习3

---
+++ 补充练习/ex53.py
@@ -0,0 +1,16 @@
+#coding:utf-8
+
+def add_function(a, b): # 定义函数类型及变量个数
+    c = a + b           # 定义函数关系
+    print c             # 定义输出的内容
+    
+
+def add(x, y):
+    return x + y
+    
+# 给变量赋值
+add_function(2, 3)
+add(3, 7)
+add(x = 3, y = 7)
+
+# 注意：少使用全局变量、代码尽可能简短运行高效
